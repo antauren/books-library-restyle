50eda0406369bd7e19c8c3b6faa0fd0efdd7b177
antauren <antauren@yandex.ru>
2020-07-17 21:50:51 +0300

    reformat

--- tululu_parser.py
+++ tululu_parser.py
@@ -16,7 +16,7 @@ def get_book_data(book_id):
     name, author = get_name_and_author_from_title(h1.text)
 
     return {'name': name,
-            'author': author
+            'author': author,
             }
 
 
