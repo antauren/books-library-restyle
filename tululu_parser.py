cd910aee6642901168d6f8a6713c960a3ef7d38f
antauren <antauren@yandex.ru>
2020-07-19 03:11:36 +0300

    reformat

--- tululu_parser.py
+++ tululu_parser.py
@@ -13,7 +13,6 @@ def get_book_data(book_id):
     soup = BeautifulSoup(response.text, 'lxml')
 
     h1 = soup.select_one('#content h1')
-
     title, author = get_name_and_author_from_title(h1.text)
 
     img_src = soup.select_one('.bookimage a img')['src']
