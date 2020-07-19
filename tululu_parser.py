b7f06398572db459a395f1d840de04e437120788
antauren <antauren@yandex.ru>
2020-07-19 03:09:15 +0300

    rename variable

--- tululu_parser.py
+++ tululu_parser.py
@@ -14,7 +14,7 @@ def get_book_data(book_id):
 
     h1 = soup.select_one('#content h1')
 
-    name, author = get_name_and_author_from_title(h1.text)
+    title, author = get_name_and_author_from_title(h1.text)
 
     img_src = soup.select_one('.bookimage a img')['src']
     img_url = urljoin('http://tululu.org', img_src)
@@ -22,7 +22,7 @@ def get_book_data(book_id):
     comments = [comment.text for comment in soup.select('.texts .black')]
     genres = [anchor.text for anchor in soup.find(text='Жанр книги:').parent.findNextSiblings('a')]
 
-    return {'name': name,
+    return {'title': title,
             'author': author,
             'img_url': img_url,
             'comments': comments,
