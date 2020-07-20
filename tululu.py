617c5d9c0c41607ffffd8dd497e71194b3174e18
antauren <antauren@yandex.ru>
2020-07-20 08:14:08 +0300

    del func download_book_from_tululu

--- tululu.py
+++ tululu.py
@@ -10,30 +10,6 @@ from tululu_parser import get_book_data
 download_folder = 'downloads'
 
 
-def download_book_from_tululu(book_id, ext='txt', allow_redirects=False, book_dir='books'):
-    book_dir = sanitize_filename(book_dir)
-
-    if ext not in {'txt', 'zip', 'jar'}:
-        raise TypeError('File must have ".txt", ".zip" or ".jar" ext.')
-
-    url = 'http://tululu.org/{ext}.php?id={book_id}'.format(book_id=book_id, ext=ext)
-
-    try:
-        book = get_book_data(book_id)
-        name = book['name']
-
-        book_name = '{}_id{}'.format(name, book_id)
-    except AttributeError:
-        book_name = book_id
-
-    file_name = '{book_name}.{ext}'.format(book_name=sanitize_filename(book_name), ext=ext)
-
-    os.makedirs(book_dir, exist_ok=True)
-    file_path = os.path.join(book_dir, file_name)
-
-    download_file(url, file_path, allow_redirects)
-
-
 def download_file(url, file_path='', allow_redirects=False):
     if not file_path:
         raise NameError('File must have name.')
